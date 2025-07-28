43b952075e29ab0be72b90a012a0420d008745bb
Bilal-coder78 <bilalsikander87@gmail.com>
2025-07-28 19:08:37 +0500

    Design the UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import ai from "../src/assets/ai.png"
+import { CiMicrophoneOn } from "react-icons/ci";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <img src={ai} alt="" id="image"/>
+      <span>I'm Shifra Your Advanced Assistant</span>
+      <button>Click here <CiMicrophoneOn /></button>
     </div>
   );
 }
