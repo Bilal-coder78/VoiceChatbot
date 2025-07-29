ca30a573d0db3608fd7e306f603c21c532933a1b
Bilal-coder78 <bilalsikander87@gmail.com>
2025-07-29 17:36:17 +0500

    CreateContext and add Speech functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css"
 import ai from "../src/assets/ai.png"
 import { CiMicrophoneOn } from "react-icons/ci";
+import { useContext } from "react";
+import { dataContext } from "./context/UserContext";
 
 
 function App() {
+  let {a} =useContext(dataContext)
   return (
     <div className="main">
       <img src={ai} alt="" id="image"/>
       <span>I'm Shifra Your Advanced Assistant</span>
-      <button>Click here <CiMicrophoneOn /></button>
+      <button onClick={()=>{
+
+      }}>Click here <CiMicrophoneOn /></button>
     </div>
   );
 }
