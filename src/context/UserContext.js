80eed63abeaa0e2b6b02fde84f312460d2237530
Bilal-coder78 <bilalsikander87@gmail.com>
2025-07-30 21:40:49 +0500

    Generate response

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,31 +1,39 @@
 import React, { createContext } from 'react'
+import main from '../gemini';
 export const dataContext = createContext()
 
-function UserContext({children}) {
-    function speak (text){
-        let text_speak = new SpeechSynthesisUtterance(text);
-        text_speak.volume = 1;
-        text_speak.rate = 1;
-        text_speak.pitch = 1;
-        text_speak.lang = "ur-GB";
-        window.speechSynthesis.speak(text_speak)
-    }
-    let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-    let recognition = new speechRecognition()
-    recognition.onresult=(e)=>{
-      let currentindex = e.resultIndex
-      let transcript = e.results[currentindex][0].transcript
-        console.log(transcript)
-    }
-    let data = {
-        speak,
-        recognition
-    }
+function UserContext({ children }) {
+  function speak(text) {
+    let text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.volume = 1;
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+    text_speak.lang = "ur-GB";
+    window.speechSynthesis.speak(text_speak)
+  }
+
+  async function airesponse(prompt){
+    let text = await main(prompt)
+    speak(text)
+  }
+
+  let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  let recognition = new speechRecognition()
+  recognition.onresult = (e) => {
+    let currentindex = e.resultIndex
+    let transcript = e.results[currentindex][0].transcript
+    console.log(transcript)
+    airesponse(transcript)
+  }
+  let data = {
+    speak,
+    recognition
+  }
   return (
     <>
-       <dataContext.Provider value={data}>
+      <dataContext.Provider value={data}>
         {children}
-        </dataContext.Provider>
+      </dataContext.Provider>
     </>
   )
 }
